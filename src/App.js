7154ab32360fae8b18ed21f9c584adb3f075f7b1
Majid Aziz <azizmajid34@gmail.com>
2023-02-28 21:17:41 +0500

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
 
 function App() {
   return (
-    <>
-      <h3>App</h3>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/single" element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
